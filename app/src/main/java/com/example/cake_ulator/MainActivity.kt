ac620e4982a287164f8bdf47f80d9af39d9ad109
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2022-12-09 22:35:39 +0400

    Set up the adapter for the dry ingredients

--- app/src/main/java/com/example/cake_ulator/MainActivity.kt
+++ app/src/main/java/com/example/cake_ulator/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.cake_ulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    val dryStuff = arrayOf("Flour", "Sugar", "Cocoa Powder")
+    val wetStuff = arrayOf("Eggs", "Milk", "Oil")
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, dryStuff)
+        val adapterB = ArrayAdapter(this, android.R.layout.simple_spinner_item, wetStuff)
+
+        var button = findViewById(R.id.button) as Button
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        adapterB.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        dryIngredients.adapter = adapter
+        dryIngredients.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
+        {
+            override fun onNothingSelected(parent: AdapterView<*>?)
+            {
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+                if(dryIngredients.selectedItemPosition==0)
+                {
+                    dryConditionA()
+                }
+                if(dryIngredients.selectedItemPosition==1)
+                {
+                    dryConditionB()
+                }
+                if(dryIngredients.selectedItemPosition==2)
+                {
+                    dryConditionC()
+                }
+            }
+        }
+    }
+
+    private fun dryConditionA()
+    {
+        Toast.makeText(this, "You selected" + dryIngredients.selectedItem, Toast.LENGTH_LONG).show()
+    }
+    private fun dryConditionB()
+    {
+        Toast.makeText(this, "You selected" + dryIngredients.selectedItem, Toast.LENGTH_LONG).show()
+    }
+    private fun dryConditionC()
+    {
+        Toast.makeText(this, "You selected" + dryIngredients.selectedItem, Toast.LENGTH_LONG).show()
     }
 }
