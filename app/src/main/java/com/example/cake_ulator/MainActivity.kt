c5bcc124515b67a8a120ac5940eaf7691196ec20
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2022-12-11 11:52:43 +0400

    Removed unnecessary code

--- app/src/main/java/com/example/cake_ulator/MainActivity.kt
+++ app/src/main/java/com/example/cake_ulator/MainActivity.kt
@@ -17,66 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, dryStuff)
-        val adapterB = ArrayAdapter(this, android.R.layout.simple_spinner_item, wetStuff)
-
-        var button = findViewById(R.id.button) as Button
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        adapterB.setDropDownViewResource(android.R.layout.simple_spinner_item)
-
-        dryIngredients.adapter = adapter
-        dryIngredients.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
-        {
-            override fun onNothingSelected(parent: AdapterView<*>?)
-            {
-            }
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
-            {
-                if(dryIngredients.selectedItemPosition==0)
-                {
-
-                }
-                if(dryIngredients.selectedItemPosition==1)
-                {
-
-                }
-                if(dryIngredients.selectedItemPosition==2)
-                {
-
-                }
-            }
-        }
-
-        wetIngredients.adapter = adapterB
-        wetIngredients.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
-        {
-            override fun onNothingSelected(parent: AdapterView<*>?)
-            {
-            }
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
-            {
-                if(wetIngredients.selectedItemPosition==0)
-                {
-
-                }
-                if(wetIngredients.selectedItemPosition==1)
-                {
-
-                }
-                if(wetIngredients.selectedItemPosition==2)
-                {
-
-                }
-            }
-        }
-
-        button.setOnClickListener{
-            if(dryIngredients.selectedItemPosition==0 && wetIngredients.selectedItemPosition==0)
-            {
-
-            }
-        }
     }
 
 }
