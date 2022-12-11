67a06247733c4cc3d5569d71e6e6246a4cea2848
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2022-12-11 17:07:00 +0400

    Added spinners for ingredients, pictures for results, adjusted cake image side
    Additional notes: odd alignment of items related to the dry ingredient options in order to make alignment look clean in the application while running the said application on devices, pictured in the following links:
    
    Image 1 - https://cdn.discordapp.com/attachments/467156925880991764/1051484510493085797/image.png
    Image 2 - https://cdn.discordapp.com/attachments/467156925880991764/1051484886772486144/Screenshot_2022-12-11-17-03-07-76_93c4344808d320ec56dd55d9491f0cab.jpg

--- app/src/main/java/com/example/cake_ulator/MainActivity.kt
+++ app/src/main/java/com/example/cake_ulator/MainActivity.kt
@@ -9,12 +9,130 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // creating arrays for every kind of ingredient
+    val ingredientsA = arrayOf("Flour", "Sugar", "Cocoa Powder")
+    val ingredientsB = arrayOf("Milk", "Egg", "Oil")
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // creating adapter values for viewing
+        val adapterD = ArrayAdapter(this, android.R.layout.simple_spinner_item, ingredientsA)
+        val adapterW = ArrayAdapter(this, android.R.layout.simple_spinner_item, ingredientsB)
+
+        // variable for the button
+        var myButton = findViewById(R.id.button) as Button
+
+        // setting the adapters
+        adapterD.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        adapterW.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        // setting the dry ingredient spinner
+        dryIngredients.adapter = adapterD
+        dryIngredients.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
+        {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+                if(dryIngredients.selectedItemPosition==0)
+                {
+                    dryPreview.setText(" " + dryIngredients.selectedItem)
+                }
+
+                else if(dryIngredients.selectedItemPosition==1)
+                {
+                    dryPreview.setText(" " + dryIngredients.selectedItem)
+                }
+
+                else if(dryIngredients.selectedItemPosition==2)
+                {
+                    dryPreview.setText(" " + dryIngredients.selectedItem)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?)
+            {
+
+            }
+
+        }
+
+        // setting the wet ingredient spinner
+        wetIngredients.adapter = adapterW
+        wetIngredients.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
+        {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+                if(wetIngredients.selectedItemPosition==0)
+                {
+                    wetPreview.setText(" " + wetIngredients.selectedItem)
+                }
+
+                else if(wetIngredients.selectedItemPosition==1)
+                {
+                    wetPreview.setText(" " + wetIngredients.selectedItem)
+                }
+
+                else if(wetIngredients.selectedItemPosition==2)
+                {
+                    wetPreview.setText(" " + wetIngredients.selectedItem)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?)
+            {
+
+            }
+
+        }
+
+        myButton.setOnClickListener {
+            if(dryIngredients.selectedItemPosition == 0 && wetIngredients.selectedItemPosition==0)
+            {
+                defaultImage.setImageResource(R.drawable.pour_milk_into_flour)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 0 && wetIngredients.selectedItemPosition==1)
+            {
+                defaultImage.setImageResource(R.drawable.img_6121)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 0 && wetIngredients.selectedItemPosition==2)
+            {
+                defaultImage.setImageResource(R.drawable.focaccia_step_1h_06532ec9_840d_498a_98a4_6c88bc0f725c_0_472x310)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 1 && wetIngredients.selectedItemPosition==0)
+            {
+                defaultImage.setImageResource(R.drawable.does_milk_have_sugar_in_it_naturally)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 1 && wetIngredients.selectedItemPosition==1)
+            {
+                defaultImage.setImageResource(R.drawable.mix_sugar_eggs_prepare_baking_cake_40267318)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 1 && wetIngredients.selectedItemPosition==2)
+            {
+                defaultImage.setImageResource(R.drawable.mix_sugar_and_olive_oil)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 2 && wetIngredients.selectedItemPosition==0)
+            {
+                defaultImage.setImageResource(R.drawable.chocolate_milk_sedimentation_5_1)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 2 && wetIngredients.selectedItemPosition==1)
+            {
+                defaultImage.setImageResource(R.drawable.ultimate_fudgy_brownies_without_eggs_19)
+            }
+
+            else if(dryIngredients.selectedItemPosition == 2 && wetIngredients.selectedItemPosition==2)
+            {
+                defaultImage.setImageResource(R.drawable.cocoa_powder)
+            }
+        }
     }
 
 }
