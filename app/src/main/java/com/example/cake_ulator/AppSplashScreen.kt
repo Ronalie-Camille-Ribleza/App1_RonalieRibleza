318490fd133def58a8874239b84860341b5dcc4a
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2022-12-11 17:39:34 +0400

    Added splash screen

---
+++ app/src/main/java/com/example/cake_ulator/AppSplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.cake_ulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppSplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_splash_screen)
+    }
+}
